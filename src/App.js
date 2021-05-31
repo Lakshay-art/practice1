763d550a159f496fb15f25c6d4f4fd8d054e476c
Lakshay-art <lakshay0173.cse19@chitkara.edu.in>
2021-05-31 20:11:12 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ import {CardGroup,Card} from 'react-bootstrap';
   this.state={down:false}}
   render(){
   return (
-    <div>
+    <div className="card-columns" style={{backgroundColor:"white"}}>
+      <Box 
+      name={'Lakshay Gupta'} 
+      rollno={'24'}/>
       <Box 
       name={'Lakshay Gupta'} 
       rollno={'24'}/>
@@ -17,9 +20,13 @@ import {CardGroup,Card} from 'react-bootstrap';
 }}
  function Box({name,rollno}){
    return(
-     <CardGroup>
-     <Card.Title className="display-4">{name}</Card.Title>
-     <Card.Text>{rollno}</Card.Text>
+     <CardGroup className="m-5 d-block">
+     <Card  style={{borderRadius:"45px"}}>
+     <Card.Body className="m-2 border-5 shadow" id="body" style={{  backgroundColor:"white",boxShadow:"5px"}}>
+     <Card.Title id="title" className="display-4">{name}</Card.Title>
+     <Card.Text id="text">{rollno}</Card.Text>
+     </Card.Body>
+     </Card>
      </CardGroup>
     
    );
